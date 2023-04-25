6700b92296e35c8a6444ee5dd62a4749072b6056
Marielle-Brasser <marielle.brasser@kpnmail.nl>
2023-04-25 14:57:37 +0200

    changes

---
+++ question.py
@@ -0,0 +1,5 @@
+question = {"At the Grote Kerk, In which timeperiod was the Grote Kerk built?" : ["Middle Ages", "Renaissance", "Industrial Age", "Early Modern Period"],
+            "At the Grote Kerk, What is bricked up into the Grote Kerk" : ["A Clock", "A Sundial", "A Mural", "A Statue"],
+            "At the Grote Kerk, Which roman numbers are on the sundial?" : ["MMIX", "CMMIVI", "MCMVVIII", "VI"],
+            "At volkspark, how many statues are there?" : ["12", "24", "6", "8"],
+            "At volkspark, what is the name of the restaurant at the park?" : ["Loetje", ""] }
