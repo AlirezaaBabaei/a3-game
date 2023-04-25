3a231e0cd3711b6a9cbdabd85c2295d5fe092b85
[Marielle Brasser] <[marielle.brasser@kpnmail.nl]>
2023-04-25 13:28:37 +0200

    Test

---
+++ locations.py
@@ -0,0 +1,5 @@
+locations = { "Grote kerk" : ["Bordjes", "De zonnewijzer"],
+              "Oude Markt" : ["Fontein", "Horeca", "Gekke kromme man"],
+               "Volkspark" : ["Standbeelden", "Restaurant", "Vijver"],
+               "Cafe Friends" : ["Games/Activeiten", "Locatie eromheen", "Radje met shots"],
+               "Rijksmuseum Twenthe" : ["Grote steen", "Vraag informatie aan medewerke"]}
