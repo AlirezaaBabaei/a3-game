5e778718d5a00bb01a7e430bb5468b56f355a74f
AlirezaaBabaei <124875076+AlirezaaBabaei@users.noreply.github.com>
2023-04-26 10:08:31 +0200

    10:08

--- main.py
+++ main.py
@@ -1,38 +1,39 @@
-# First welcome message Quiz
-# How many players (at least 2)
-# On an other computer you would like to have also another group
-# Asking for the teams names
-# Asking about the names of the players and add them into the team.
-# all of the players are in one team
-# Ask each group about 10 quations
-# Calculate how many quations are correct
-# Hardcode 2 groups with names and calculate the score
-# Printing the winner
-
+import pygame
 from read_file import generate_questions
 from locations import location_finder
 from calculate_score import calculate_score
 
 def main():
-    print("Welcome to our Quiz")
+    pygame.init()
+    pygame.mixer.music.load("intro.mp3")
+    pygame.mixer.music.play()
+    print("\nWelcome to our Quiz\n")
     while True:
         players_num = input("How many members are in your team? (at least 2): ")
         try:
             players_num = int(players_num)
-        except:
+            if players_num < 2:
+                raise ValueError
+        except ValueError:
+            print("Error: Please enter a number greater than or equal to 2.")
             continue
-        if players_num >= 2:
-            team = {}
-            team_name = input("What's the team name: ")
-            players = []
-            for i in range(1, players_num + 1):
-                name = input(f"What is the name of player {i}: ")
-                players.append(name)
-            team[team_name] = players
+        
+        team = {}
+        team_name = input("What's the team name: ")
+        players = []
+        for i in range(1, players_num + 1):
+            name = input(f"What is the name of player {i}: ")
+            players.append(name)
+        team[team_name] = players
+        
         location_finder()
         questions = generate_questions()
         score = calculate_score(questions)
         print(f"{team_name}'s score is {score}%")
+        
+        pygame.init()
+        pygame.mixer.music.load("end.mp3")
+        pygame.mixer.music.play()
         break
 
 if __name__ == "__main__":
